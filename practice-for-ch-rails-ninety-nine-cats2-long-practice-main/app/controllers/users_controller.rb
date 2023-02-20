ffc12d438e13eb04b97753fe89c32fd4a12a5e5d
Sean Abbas <sean.abbas33@gmail.com>
2023-02-20 17:08:38 -0500

    create views

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save 
-            redirect_to user_url(@user)
+            render json: 'User Created!'
         else
             render :new
         end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.require(:user).permit(:username, :password_digest, :session_token)
+        params.require(:user).permit(:username, :password)
     end
     
 end
